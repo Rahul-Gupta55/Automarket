7917c6a1196a0d6c083eacaafc82d98506e9aa7f
ShepelevAndrew <shepelev.andrew.101@gmail.com>
2022-12-02 19:17:50 +0300

    Added DAL part of project + connect service dbcontext + string connect to db in appsettings

---
+++ Automarket.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Automarket.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automarket.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Car>
+    }
+}
--- Automarket/Program.cs
+++ Automarket/Program.cs
@@ -1,7 +1,12 @@
+using Automarket.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSQLServer(connection));
 
 var app = builder.Build();
 
