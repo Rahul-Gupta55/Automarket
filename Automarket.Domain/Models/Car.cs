9100ffa3613faf8691db96f605489f43615fb68a
ShepelevAndrew <shepelev.andrew.101@gmail.com>
2022-12-02 18:42:52 +0300

    Added Domain part of project

---
+++ Automarket.Domain/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using Automarket.Domain.Enum;
+
+namespace Automarket.Domain.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateCreate { get; set; }
+        public CarTypes CarType { get; set; }
+    }
+}
